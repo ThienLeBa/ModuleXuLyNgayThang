bc3775769ca5a3194d4371449a702063d68e857c
Thien Le <Thienlb15411@st.uel.edu.vn>
2019-12-12 18:45:28 +0700

    Commit setting project

---
+++ XuLyNgayThang/src/thienle/ifactory/TestNgayThang.java
@@ -0,0 +1,12 @@
+package thienle.ifactory;
+
+public class TestNgayThang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Lê Bá Thiền");
+
+	}
+
+}
